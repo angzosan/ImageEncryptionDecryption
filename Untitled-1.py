e2f6bc0dd3780cbba78064c63b29d7f6c91689d7
Anna G. Zosan <118728873+angzosan@users.noreply.github.com>
2023-01-24 15:42:43 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,40 @@
+import io
+from PIL import Image
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+#path = input("Enter the path of the image for encryption : ")
+key = input( "Enter they key for the encryption : ")
+key = int(key) 
+path = 'image.png'
+
+with open(path, "rb") as image:
+  f = image.read()
+  imagByt = bytearray(f)
+
+#print(imagByt)
+encryImage = [ 0 for i in range(0, len(imagByt))]
+for i in range(0, len(imagByt)):
+    encryImage[i] = imagByt[i]^key
+
+#print(encryImage)
+encryImage = bytearray(encryImage)
+#imag = Image.frombuffer('L', , encryImage, 'raw', 'L', 0, 1)
+#img.show()
+#encryImage = bytearray(encryImage)
+
+
+print('The image has been encrypted.')
+print(encryImage)
+
+
+print("Decryption process begins : ")
+decryImage = [ 0 for i in range(0, len(encryImage))]
+for i in range(0, len(imagByt)):
+    decryImage[i] = encryImage[i]^key
+
+
+print('The image has been decrypted.')
+print(decryImage)
